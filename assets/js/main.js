741e360f2c457e4cdaafbb54e996fdd932909c19
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-10-30 10:12:13 -0300

    Correccion en la tabla del carrito

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,9 +44,9 @@ async function petshop() {
 
             tablaCarritoTotal.innerHTML = `
         <tr>
-            <td colspan="2" class="text-center fw-semibold">Total :</td>
-            <td colspan="4" class="fw-semibold"></td>
-            <td class="fw-semibold">$${total}</td>
+            <td colspan="1" class="text-center fw-semibold">Total :</td>
+            <td colspan="5" class="fw-semibold"></td>
+            <td class="text-center fw-semibold">$${total}</td>
         </tr>
         `
         }
@@ -208,8 +208,8 @@ async function petshop() {
                 <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"></path>
             </svg></button>     
             </td>
-            <td id='cantidad-${array._id}'data-value="${array.stock}">${array.cantidad}</td>
-            <td><button id="mas-${array._id}" class="border-0 bg-transparent botonCambioMas"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4f706f" class="bi bi-plus-square product-name-add" style="z-index: 2;" viewBox="0 0 16 16">
+            <td class="text-black text-center fw-semibold" id='cantidad-${array._id}'data-value="${array.stock}">${array.cantidad}</td>
+            <td class="text-black text-center fw-semibold"><button id="mas-${array._id}" class="border-0 bg-transparent botonCambioMas"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#4f706f" class="bi bi-plus-square product-name-add" style="z-index: 2;" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
             </svg></button></td>
